192fe45f2c66dc23cd553ccc8365f5aedb654a73
ectaw6d <Andrew.Palet@McKesson.com>
2021-03-15 15:27:17 -0500

    added method for buttonVisibility

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -76,6 +76,14 @@ class StoryBrain {
   void restart() {
     _storyNumber = 0;
   }
+
+  bool buttonShouldBeVisible() {
+    if (_storyNumber < 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
